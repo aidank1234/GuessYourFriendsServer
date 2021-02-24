1f82ec0b52c39f825ac0f5f123fa8c594357c94f
aidankaiser <apkaiser@wisc.edu>
2021-02-24 14:42:42 -0600

    Added address field to user

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,12 +1,17 @@
+const uniqueValidator = require('mongoose-unique-validator');
+
 module.exports = mongoose => {
     const userSchema = mongoose.Schema(
         {
             username: {type: String, unique: true, required: true},
             password: {type: String, unique: false, required: true},
-            phoneNumber: {type: String, unique: true, required: true}
+            phoneNumber: {type: String, unique: true, required: true},
+            address: {type: String, unique: true, required: true}
         },
         {timestamps: true}
     );
+
+    userSchema.plugin(uniqueValidator, { message: '{PATH} has already been used.' });
 
     const User = mongoose.model(
         "user",
